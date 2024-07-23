5538f0c187fabed4e043d8335affc5c348b6fc1d
Trophy198 <spsp8426@naver.com>
2024-07-23 21:53:33 +0900

    refactor: 유저 정보 api 수정 및 로그아웃 api 추가

--- src/lib/api/auth/api.ts
+++ src/lib/api/auth/api.ts
@@ -1,7 +1,13 @@
 import axios from '../config';
 
 export const authenticateUser = async (provider: string, code: string) => {
-  const url = `/auth/login/${provider}?code=${code}`;
+  const url = `/auth/${provider}/callback?code=${code}`;
   const response = await axios.get(url);
+  return response.data;
+};
+
+export const logoutUser = async (token: string) => {
+  const url = `/auth/logout/${token}`;
+  const response = await axios.patch(url);
   return response.data;
 };
--- src/lib/api/user/api.ts
+++ src/lib/api/user/api.ts
@@ -2,6 +2,10 @@ import authAxios from '../authConfig';
 
 export const getUserProfile = async () => {
   const url = '/members/my-page';
-  const response = await authAxios.get(url);
-  return response.data;
+  try {
+    const response = await authAxios.get(url);
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.message || '유저정보 가져오기에 실패하였습니다.');
+  }
 };
