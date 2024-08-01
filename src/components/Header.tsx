ddbe70eab93edf0c5edb3c7fcf39e2451d4a0a3b
Trophy198 <spsp8426@naver.com>
2024-08-01 23:27:28 +0900

    refactor: 변경된 컴포넌트 네이밍으로 인한 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
 import ThemeSwitcher from './theme/Switcher';
 import { Link } from 'react-router-dom';
 import Image from './common/Image';
-import DropdownMenu from './mycontents/DropdownMenu';
+import ProfileDropdownMenu from './mycontents/ProfileDropdownMenu';
 import useApi from '../hooks/useApi';
-import { getUserProfile } from '../lib/api/user/api';
+import { getMemberProfile } from '../lib/api/member/api';
 import { getItem, removeItem } from '../utils/localStorageUtil';
 import { logoutUser } from '../lib/api/auth/api';
 import { useEffect, useState } from 'react';
 
 const Header = () => {
-  const { data, loading } = useApi(getUserProfile);
+  const { data, loading } = useApi(getMemberProfile);
   const [user, setUser] = useState(data);
 
   useEffect(() => {
@@ -34,7 +34,7 @@ const Header = () => {
   };
 
   return (
-    <header className="w-full sticky top-0 flex border-[#6E6E82] border-b-2 p-5 justify-between backdrop-blur-sm">
+    <header className="w-full z-[50] sticky top-0 flex border-[#6E6E82] border-b-2 p-5 justify-between backdrop-blur-sm">
       <Link className="flex justify-center items-center" to="/">
         <Image src="/assets/image/logo.png" alt="mainlogo" className="w-10 h-10 sm:w-8 sm:h-8" />
         <h1 className="hidden sm:block text-xl sm:text-2xl ml-2">
@@ -47,7 +47,7 @@ const Header = () => {
         {loading ? (
           <div className="w-[40px] h-[40px]"></div>
         ) : user ? (
-          <DropdownMenu user={user} handleLogout={handleLogout} />
+          <ProfileDropdownMenu user={user} handleLogout={handleLogout} />
         ) : (
           <Link
             className="flex items-center disabled:opacity-50 disabled:hover:opacity-50 justify-center ring-none rounded-lg shadow-lg font-semibold py-2 px-4 font-dm focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-400 border-blue-700 disabled:border-0 disabled:bg-blue-500 disabled:text-white ring-white text-white border-b-4 transition duration-300 ease-in-out hover:bg-blue-500 hover:border-blue-800 hover:shadow-none active:shadow-inner active:bg-blue-800 active:text-gray-300 focus-visible:outline-blue-500 text-sm sm:text-base dark:hover:bg-blue-800 dark:bg-blue-700 dark:border-blue-700 dark:border-b-blue-900"
